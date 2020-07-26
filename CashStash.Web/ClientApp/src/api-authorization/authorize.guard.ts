855c18983b7e3e3826d1f430a53a088bdfc518be
Borys Malynovs'kyi <malinovsky.post@gmail.com>
2020-07-26 14:43:00 +0300

    authorization module formatting

--- CashStash.Web/ClientApp/src/api-authorization/authorize.guard.ts
+++ CashStash.Web/ClientApp/src/api-authorization/authorize.guard.ts
@@ -1,30 +1,29 @@
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthorizeService } from './authorize.service';
-import { tap } from 'rxjs/operators';
+
 import { ApplicationPaths, QueryParameterNames } from './api-authorization.constants';
+import { AuthorizeService } from './authorize.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthorizeGuard implements CanActivate {
-  constructor(private authorize: AuthorizeService, private router: Router) {
-  }
-  canActivate(
-    _next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.authorize.isAuthenticated()
-        .pipe(tap(isAuthenticated => this.handleAuthorization(isAuthenticated, state)));
-  }
+	constructor(private authorize: AuthorizeService, private router: Router) { }
 
-  private handleAuthorization(isAuthenticated: boolean, state: RouterStateSnapshot) {
-    if (!isAuthenticated) {
-      this.router.navigate(ApplicationPaths.LoginPathComponents, {
-        queryParams: {
-          [QueryParameterNames.ReturnUrl]: state.url
-        }
-      });
-    }
-  }
+	public canActivate(_next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+		return this.authorize.isAuthenticated()
+			.pipe(tap(isAuthenticated => this.handleAuthorization(isAuthenticated, state)));
+	}
+
+	private handleAuthorization(isAuthenticated: boolean, state: RouterStateSnapshot) {
+		if (!isAuthenticated) {
+			this.router.navigate(ApplicationPaths.LoginPathComponents, {
+				queryParams: {
+					[QueryParameterNames.ReturnUrl]: state.url
+				}
+			});
+		}
+	}
 }
